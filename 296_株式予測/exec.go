6d470b8a243f88d64e018f735bb171e37af83e17
kazu24z <progra.taro2021@gmail.com>
2025-10-13 16:53:09 +0900

    296_株式予測（スタック）

--- 296_株式予測/exec.go
+++ 296_株式予測/exec.go
@@ -3,32 +3,28 @@ package main
 func dailyStockPrice(stocks []int32) []int32 {
 	length := int32(len(stocks))
 
-	// 結果配列
-	result := make([]int32, 0)
+	// 結果配列を全部0で初期化
+	result := make([]int32, length)
 
-	// 配列の要素ごとに回す処理
-	for i := int32(0); i < length; i++ {
+	// スタック（インデックスを入れる）
+	stack := []int32{}
 
-		if i == length-1 {
-			result = append(result, 0)
-			break
+	// 配列を右から左に走査
+	for i := length - 1; i >= 0; i-- {
+		// スタックトップが現在の株価以下なら全部捨てる
+		for len(stack) > 0 && stocks[stack[len(stack)-1]] <= stocks[i] {
+			stack = stack[:len(stack)-1] // pop
 		}
-		for j := i + 1; j < length; j++ {
-			// stocks[i] < stocks[j]になったときのjの値が上昇まで待つ日
-			if stocks[i] < stocks[j] {
-				result = append(result, j-i)
-				break
-			}
 
-			// 最後のループになってもstocks[i] < stocks[j]にならない場合
-			if stocks[i] >= stocks[j] && j == length-1 {
-				result = append(result, 0)
-				break
-			}
+		// スタックに何か残ってたら、それが答え
+		if len(stack) > 0 {
+			result[i] = stack[len(stack)-1] - i
 		}
+		// スタックが空なら result[i] = 0 のまま
+
+		// 今日をスタックに追加
+		stack = append(stack, i)
 	}
 
 	return result
 }
-
-// stack使わなかった...
