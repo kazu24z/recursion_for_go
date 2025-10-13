fd13299361752deffedafdfb69de275b64ebfec3
kazu24z <progra.taro2021@gmail.com>
2025-10-13 19:59:21 +0900

    189_回文連結リスト_修正

--- 189_回文連結リスト/exec.go
+++ 189_回文連結リスト/exec.go
@@ -6,7 +6,7 @@ type SinglyLinkedListNode struct {
 }
 
 func palindromeLinkedList(head *SinglyLinkedListNode) bool {
-	// ノード数をカウント
+	// ノードの数
 	current := head
 	counter := int32(0)
 	for current != nil {
@@ -14,28 +14,43 @@ func palindromeLinkedList(head *SinglyLinkedListNode) bool {
 		counter++
 	}
 
+	isEven := (counter%2 == 0) // 偶数ならtrue
+	var skipNum int32
+
+	if isEven {
+		skipNum = 1
+	} else {
+		skipNum = 2
+	}
+
 	temp := head
+	// 空のスタック配列
 	stack := []int32{}
 
-	// 前半をスタックに積む
-	for i := int32(0); i < counter/2; i++ {
+	// 先頭からcounterの半分までをスタックに入れる
+	for i := int32(1); i <= counter/2; i++ {
 		stack = append(stack, temp.data)
 		temp = temp.next
 	}
 
-	// 奇数の場合は真ん中をスキップ
-	if counter%2 == 1 {
+	if !isEven {
+		// 奇数の場合、頂点の値をスキップ
 		temp = temp.next
 	}
 
-	// 後半をスタックと比較
-	for i := int32(0); i < counter/2; i++ {
-		if len(stack) == 0 || stack[len(stack)-1] != temp.data {
-			return false
+	// 後半分に対して、スタックの先頭と比較し、値が一致し続けるかをチェック
+	for i := counter/2 + skipNum; i <= counter; i++ {
+		if stack[len(stack)-1] == temp.data {
+			// スタックから値をpopしていく
+			stack = stack[:len(stack)-1]
 		}
-		stack = stack[:len(stack)-1]
 		temp = temp.next
 	}
+	// スタックが空なら回文
+	if len(stack) == 0 {
+		return true
+	}
 
-	return len(stack) == 0
+	// それ以外の場合はfalseを返す
+	return false
 }
